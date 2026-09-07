6bca2c234655ccd4be7bc743c470a3897e50fdf0
KT3746 <tks.front463@passmail.com>
2026-09-07 04:03:33 +0000

    fix: cache-bust ?v=minimap30

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-import { CARS, TRACKS, UPGRADES, PRIZE, POINTS, DRIVERS, QUALIFY } from "./data.js?v=map30";
-import { AudioBus } from "./audio.js?v=map30";
-import { GameEngine } from "./engine.js?v=map30";
-import { getModo } from "./modo.js?v=map30";
+import { CARS, TRACKS, UPGRADES, PRIZE, POINTS, DRIVERS, QUALIFY } from "./data.js?v=minimap30";
+import { AudioBus } from "./audio.js?v=minimap30";
+import { GameEngine } from "./engine.js?v=minimap30";
+import { getModo } from "./modo.js?v=minimap30";
 
 const SAVE_KEY = "relampago-save";
 
