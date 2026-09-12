90c404296d23f0f88b73986c5a0ea8ef3f49ddad
KT3746 <tks.front463@passmail.com>
2026-09-12 21:02:19 +0000

    ui: cache-bust nosombtn2 engine

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,4 @@
-import { CARS, DRIVERS, TRACKS, applyUpgrades } from "./data.js?v=nosombtn1";
+import { CARS, DRIVERS, TRACKS, applyUpgrades } from "./data.js?v=nosombtn2";
 
 const SEG = 200;
 const ROAD = 2100;
